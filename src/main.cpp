76b517eb80ce6db8e8709f710adcd26b1a26489b
JungGilSoo <edwardjung@jtwoc.com>
2021-10-02 00:39:56 +0900

    222

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 
 #include <QApplication>
 
+void serialConfigInit();
+
 int main(int argc, int *argv)
 {
 
+    serialConfigInit();
     QApplication a(argc, argv);
     MainWindows w;
     w.show();
