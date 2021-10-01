37b9d9f18f05c22ee2db6ccc4931a846f1e45dd2
JungGilSoo <edwardjung@jtwoc.com>
2021-10-02 01:48:46 +0900

    qInstallMesageHandler

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,37 @@
 
 void serialConfigInit(); // What is this?
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    const char *file = context.file ? context.file : "";
+    const char *function = context.function ? context.function : "";
+    QTime curtime = QTime::currentTime();
+
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "[%s:%03d] %s\n",curtime.toString().toStdString().c_str(), curtime.msec(),localMsg.constData());
+        break;
+    case QtInfoMsg:
+        fprintf(stderr, "[%s:%d] Info %s\n",curtime.toString().toStdString().c_str(), curtime.msec(),localMsg.constData());
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), file, context.line, function);
+        break;
+    }
+}
+
 int main(int argc, int *argv)
 {
 
     
-
+    qInstallMessageHandler(myMessageOutpur); // Message Handler Registration
     serialConfigInit(); //
     QApplication a(argc, argv);
     MainWindow w;
