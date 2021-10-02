c2bf257db57408e28aa3fa5bb13929cc9550b615
JungGilSoo <edwardjung@jtwoc.com>
2021-10-02 12:09:31 +0900

    j2c.png

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,13 +1,26 @@
 
 
 #include <QMainWindow>
+#include <QSettings>
 
 
 class MainWindow : public QMainWind
 {
 
+public:
+    MainWindow (QWidgt *parent = nullptr);
+    ~MainWindow();
+    Ui::MainWindow *ui;
+
+    QString appPath() { return m_appPath; }
+
+    int mWindowRect;
+    int mDisplayMode;
+
 private:
 
     QString m_appPath;
 
+
+    void configInit();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,7 @@
 
+#include <QDir>
+#include <Qdebug>
+
 #include <opencv2/core/utils/logger.hpp>
 
 MainWindow* theMainWindow = 0;
@@ -27,4 +30,95 @@ MainWindow::MainWindow(QWidget *parent)
 
     // from here
     m_appPath = QDir::currentPath();
+    qDebug() << "MainWindow::MainWindow" << m_appPath;
+
+    // swchoi
+
+    configInit();
+
+}
+
+
+void MainWindow::coonfigInit()
+{
+    qDebug("Config Start");
+
+    QString filename = appPath() +"/config.ini";
+    m_configIni = new QSettings(filename, QSettings::IniFormat);
+
+
+    // Using screenGeometry, the start coordinates of the Monitor obtains.
+    QRect rec = QApplication::desktop()->screenGeometry();
+    int height = rect.height();
+    int width = rect.width();
+    qDebug("[Mainwin] WindowSizw %dx%d",width,height);
+    if( width > height ) {
+        mWindowRec = 1;
+        if( height > 600 ) {
+            mWindowRec = 2;
+            this->setGeometry(0,0,720,1080);
+        }
+        else {
+            this->setGeometry(0,0,1024,600);
+        }
+        if( mDisplayMode == 1 ) {
+            mWindowRec = 0;
+            this->setGeometry(0,0,480,800);
+        }
+    }
+    else {
+        mWindowRec = 0;
+        this->setGeometry(0,0,720,1350);
+    }
+
+    mWindowRec = 0; // Whatls the code above?
+    this->setGeometry(0,0,720,1350);
+
+    if ( mWindowRec == 1 ) {
+	    qDebug("Set mWindowRec 1");
+        ui->icon->move(1024-100,20);
+        ui->labelId->move(210,180);
+        ui->labelName->move(210,210);
+        ui->labelTime1->move(750,120);
+        ui->labelTime2->move(750,70);
+        ui->fullScreen->resize(1024,600);
+        ui->fullScreen->move(0,0);
+        ui->eyeScreen->move(1024-280,600-240);
+        ui->eyeScreen2->move(1024-280,600-240-200);
+    }
+    else if ( mWindowRec == 2 ) {
+	    qDebug("Set mWindowRec 2");
+        ui->icon->move(1024-100,20);
+        ui->labelId->move(210,240);
+        ui->labelName->move(210,270);
+        ui->labelTime1->move(750,120);
+        ui->labelTime2->move(750,70);
+        ui->fullScreen->resize(720,720);
+        ui->fullScreen->move(0,1080-720);
+        ui->eyeScreen->move(0,1080-160);
+        ui->eyeScreen2->move(0,1080-320);
+	    mWindowRec = 0;
+    }
+    else {
+        qDebug("Set mWindowRoc 0");
+        ui->icon->move(720-140,0);
+        ui->labelId->resize(120,50);
+
+        ui->fullScreen->resize(720,1230);
+        ui->fullScreen->move(0,50);
+        ui->labelTime1->move(560,1220);
+        ui->labelTime2->move(30,1220);
+
+        ui->eyeScreen->resize(160,120);
+        ui->eyeScreen->move(15,1280-120-50);
+        ui->eyeScreen2->move(15,1280-120-50);
+
+        ui->labelID->move(150,630);
+        ui->labelName->move(210,270);
+    }
+
+    cv::VideoCapture icon;
+    cv::Mat pic;
+    QPixmap s_ing;
+    icon.open("./resource/j2c.png");
 }
